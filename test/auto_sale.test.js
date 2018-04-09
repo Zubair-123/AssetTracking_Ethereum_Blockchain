d4f93ba842c72510710d9fc4c1b1416bcfd806c5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-09 11:34:45 +0500

    contract successfully deployed

--- test/auto_sale.test.js
+++ test/auto_sale.test.js
@@ -2,24 +2,34 @@ const assert = require('assert');
 
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const {interface, bytcode} = require('../compile');
+const {interface, bytecode} = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let automobile_Sale;
 
 beforeEach( async () => {
     
     accounts = await web3.eth.getAccounts();
-    
+        // then deploy the contract after getting the accounts
+    automobile_Sale = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments : ['Toyota','Japan']
+        })
+        .send({ 
+            from: accounts[1], 
+            gas: '3000000'
+        });
 });
 
 describe('Testing Automobile Contract', () => {
-    it('getting the accounts', () => { 
-        console.log(accounts)
-    });
+    // it('getting the accounts', () => { 
+    //     assert.ok(accounts[0])
+    // });
     
     it('contract deployed at', () => {
-        assert.ok(Automobile_Sale.options.address);
+        assert.ok(automobile_Sale.options.address);
     });
 });
