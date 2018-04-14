6e3bad51b1f20fa010d73730d88d6c82f9cdc7f3
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-14 13:02:52 +0500

    test cases updated

--- test/auto_sale.test.js
+++ test/auto_sale.test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const {interface, bytecode} = require('../compile');
@@ -22,14 +21,23 @@ beforeEach( async () => {
             from: accounts[0],
             gas: '3000000'
         });
-    await automobile_Sale.methods.createAsset(11,'Red','Diesel','Japan','25-05-1996').send({
-        from: accounts[0],
-        gas: '3000000'
+    await automobile_Sale.methods.createAsset(11,'Red', 'Diesel', 'Japan','25-05-1996').send({
+            from: accounts[0],
+            gas: '3000000'
     });
-    await automobile_Sale.methods.createAsset(22,'White','Petrol','Japan','26-05-1996').send({
-        from: accounts[0],
-        gas: '3000000'
+    await automobile_Sale.methods.createAsset(12,'Blue', 'Petrol', 'Japan','26-05-1996').send({
+            from: accounts[0],
+            gas: '3000000'
     });
+    await automobile_Sale.methods.createAsset(21,'Pink', 'Diesel', 'Japan','25-05-1996').send({
+            from: accounts[0],
+            gas: '3000000'
+    });
+    await automobile_Sale.methods.createAsset(22,'White', 'Petrol', 'Japan','26-05-1996').send({
+            from: accounts[0],
+            gas: '3000000'
+    });
+ 
 });
 
 describe('Testing Automobile Contract', () => {
@@ -41,22 +49,18 @@ describe('Testing Automobile Contract', () => {
         assert.ok(automobile_Sale.options.address);
     });
     
-    it('create and get all cars manufactured by manufacturer', async () => {
-        
+    it('new assets created', async () =>{
 
-        let cars = await automobile_Sale.methods.getListOfAssetsOwnedByManufacturer().call({
-            from: accounts[0],
-        }).then( result => {
-            return result
-        })
+    })
+    it('create and get all cars manufactured by manufacturer', async () => {        
+
+        const cars = await automobile_Sale.methods.getListOfAssetsOwnedByManufacturer().call()
         console.log(cars);
-        assert.ok(cars);
+        // assert.ok(cars);
     });
 
     it('get specific cars manufactured by manufacturer', async () => {
         
-        
-
         await automobile_Sale.methods.getSpecificCar(11).call({
             from: accounts[0],
         }).then( console.log)
@@ -72,46 +76,48 @@ describe('Testing Automobile Contract', () => {
 
     it('Transfer ownership of the car', async () => {
 
-        await automobile_Sale.methods.transferToOwner(accounts[1].toString(),"Ibad",11,"Karachi").send({
+        await automobile_Sale.methods.transferToOwner(accounts[0],accounts[1].toString(),"Ibad",11,"Karachi").send({
             from: accounts[0],
             gas: "3000000",
             value: web3.utils.toWei('1','ether')
-        }).then(console.log)
+        }).then(console.log);
             
-        await automobile_Sale.methods.transferToOwner(accounts[2].toString(),"Shahzaib",11,"Karachi").send({
-            from: accounts[1],
-            gas: "3000000",
-            value: web3.utils.toWei('1','ether')
-        }).then(console.log)
+        // await automobile_Sale.methods.transferToOwner(accounts[1],accounts[2].toString(),"Shahzaib",11,"Karachi").send({
+        //     from: accounts[1],
+        //     gas: "3000000",
+        //     value: web3.utils.toWei('1','ether')
+        // }).then(console.log);
 
-        await automobile_Sale.methods.getCurrentOwnerOfAsset(11)
-            .call({
+
+        await automobile_Sale.methods.getCurrentOwnerOfAsset(11).call({
                 from: accounts[0]
-            })
-            .then(
-                console.log
-            )
-        await automobile_Sale.methods.getPreviousOwnerOfAsset(11)
-            .call({
-                from: accounts[0],
-            })
-            .then( console.log)
+            }).then( console.log)
+
+        // await automobile_Sale.methods.getPreviousOwnerOfAsset(11).call({
+        //         from: accounts[0],
+        //     }).then( console.log)
     });
     
     it('get current owner of car', async () => {
         await automobile_Sale.methods.getCurrentOwnerOfAsset(11).call({
             from: accounts[0],
-            // gas: "3000000"
+            gas: "3000000"
         }).then( console.log)
     });
 
+    it('get manufacturer details of car', async () => {
+        await automobile_Sale.methods.getManufacturerName().call({
+            from: accounts[0],
+            // gas: "3000000"
+        }).then( console.log)
+    });
     // it('get previous owner of the cars', async () => {
     //     await automobile_Sale.methods.getPreviousOwnerOfAsset(11).call({
     //         from: accounts[0],
     //         // gas: "3000000"
     //     }).then( console.log)
         
-    //     // assert.ok('got it');
+    // assert.ok('got it');
     // })
 
 });
